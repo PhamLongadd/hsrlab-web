9b3fa529f47ab156a3aeb56e440f4ec38cb30538
Pham Minh Tuan <phmtuan313@gmail.com>
2023-11-30 01:13:53 +0700

    Add meta tag to fix mixed content

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
